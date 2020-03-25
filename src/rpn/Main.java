4379e9e15a42d97f9596b85c84a96a6be78ebfbb
PKS <kirill_nn@inbox.ru>
2020-03-25 08:33:56 +0300

    +

--- src/rpn/Main.java
+++ src/rpn/Main.java
@@ -3,125 +3,125 @@ package rpn;
 import java.util.*;
 
 class ExpressionParser {
-  private static String operators = "+-*/^";
-  private static String delimiters = "() " + operators;
-  public static boolean flag = true;
+    private static String operators = "+-*/^";
+    private static String delimiters = "() " + operators;
+    public static boolean flag = true;
 
-  private static boolean isDelimiter(String token) {
-    if (token.length() != 1) return false;
-    for (int i = 0; i < delimiters.length(); i++) {
-      if (token.charAt(0) == delimiters.charAt(i)) return true;
+    private static boolean isDelimiter(String token) {
+        if (token.length() != 1) return false;
+        for (int i = 0; i < delimiters.length(); i++) {
+            if (token.charAt(0) == delimiters.charAt(i)) return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  private static boolean isOperator(String token) {
-    if (token.equals("u-")) return true;
-    for (int i = 0; i < operators.length(); i++) {
-      if (token.charAt(0) == operators.charAt(i)) return true;
+    private static boolean isOperator(String token) {
+        if (token.equals("u-")) return true;
+        for (int i = 0; i < operators.length(); i++) {
+            if (token.charAt(0) == operators.charAt(i)) return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  private static int priority(String token) {
-    if (token.equals("(")) return 1;
-    if (token.equals("+") || token.equals("-")) return 2;
-    if (token.equals("*") || token.equals("/") || token.equals("^")) return 3;
-    return 4;
-  }
+    private static int priority(String token) {
+        if (token.equals("(")) return 1;
+        if (token.equals("+") || token.equals("-")) return 2;
+        if (token.equals("*") || token.equals("/") || token.equals("^")) return 3;
+        return 4;
+    }
 
-  public static List<String> parse(String infix) {
-    List<String> listValue = new ArrayList<>();
-    Deque<String> stackOperators = new ArrayDeque<>();
-    StringTokenizer tokenizer = new StringTokenizer(infix, delimiters, true);
-    String prev = "";
-    String curr;
-    while (tokenizer.hasMoreTokens()) {
-      curr = tokenizer.nextToken();
-      if (!tokenizer.hasMoreTokens() && isOperator(curr)) {
-        System.out.println("Некорректное выражение.");
-        flag = false;
-        return listValue;
-      }
-      if (curr.equals(" ")) continue;
-      else if (isDelimiter(curr)) {
-        if (curr.equals("(")) stackOperators.push(curr);
-        else if (curr.equals(")")) {
-          while (!stackOperators.peek().equals("(")) {
-            listValue.add(stackOperators.pop());
-            if (stackOperators.isEmpty()) {
-              System.out.println("Скобки не согласованы.");
-              flag = false;
-              return listValue;
+    public static List<String> parse(String infix) {
+        List<String> listValue = new ArrayList<>();
+        Deque<String> stackOperators = new ArrayDeque<>();
+        StringTokenizer tokenizer = new StringTokenizer(infix, delimiters, true);
+        String prev = "";
+        String curr;
+        while (tokenizer.hasMoreTokens()) {
+            curr = tokenizer.nextToken();
+            if (!tokenizer.hasMoreTokens() && isOperator(curr)) {
+                System.out.println("Некорректное выражение.");
+                flag = false;
+                return listValue;
             }
-          }
-          stackOperators.pop();
-        } else {
-          if (curr.equals("-") && (prev.equals("") || (isDelimiter(prev) && !prev.equals(")")))) {
-            curr = "u-";
-          } else {
-            while (!stackOperators.isEmpty() && (priority(curr) <= priority(stackOperators.peek()))) {
-              listValue.add(stackOperators.pop());
+            if (curr.equals(" ")) continue;
+            else if (isDelimiter(curr)) {
+                if (curr.equals("(")) stackOperators.push(curr);
+                else if (curr.equals(")")) {
+                    while (!stackOperators.peek().equals("(")) {
+                        listValue.add(stackOperators.pop());
+                        if (stackOperators.isEmpty()) {
+                            System.out.println("Скобки не согласованы.");
+                            flag = false;
+                            return listValue;
+                        }
+                    }
+                    stackOperators.pop();
+                } else {
+                    if (curr.equals("-") && (prev.equals("") || (isDelimiter(prev) && !prev.equals(")")))) {
+                        curr = "u-";
+                    } else {
+                        while (!stackOperators.isEmpty() && (priority(curr) <= priority(stackOperators.peek()))) {
+                            listValue.add(stackOperators.pop());
+                        }
+                    }
+                    stackOperators.push(curr);
+                }
+
+            } else {
+                listValue.add(curr);
             }
-          }
-          stackOperators.push(curr);
+            prev = curr;
         }
 
-      } else {
-        listValue.add(curr);
-      }
-      prev = curr;
-    }
-
-    while (!stackOperators.isEmpty()) {
-      if (isOperator(stackOperators.peek())) listValue.add(stackOperators.pop());
-      else {
-        System.out.println("Скобки не согласованы.");
-        flag = false;
+        while (!stackOperators.isEmpty()) {
+            if (isOperator(stackOperators.peek())) listValue.add(stackOperators.pop());
+            else {
+                System.out.println("Скобки не согласованы.");
+                flag = false;
+                return listValue;
+            }
+        }
         return listValue;
-      }
     }
-    return listValue;
-  }
 }
 
 class Ideone {
-  public static Double calc(List<String> value) {
-    Deque<Double> stackOperators = new ArrayDeque<>();
-    for (String x : value) {
-      if (x.equals("+")) {
-        stackOperators.push(stackOperators.pop() + stackOperators.pop());
-      } else if (x.equals("^")) {
-        Double b = stackOperators.pop(), a = stackOperators.pop();
-        stackOperators.push(Math.pow(a, b));
-      } else if (x.equals("-")) {
-        Double b = stackOperators.pop(), a = stackOperators.pop();
-        stackOperators.push(a - b);
-      } else if (x.equals("*")) {
-        stackOperators.push(stackOperators.pop() * stackOperators.pop());
-      } else if (x.equals("/")) {
-        Double b = stackOperators.pop(), a = stackOperators.pop();
-        stackOperators.push(a / b);
-      } else if (x.equals("u-")) {
-        stackOperators.push(-stackOperators.pop());
-      } else {
-        stackOperators.push(Double.valueOf(x));
-      }
+    public static Double calc(List<String> value) {
+        Deque<Double> stackOperators = new ArrayDeque<>();
+        for (String x : value) {
+            if (x.equals("+")) {
+                stackOperators.push(stackOperators.pop() + stackOperators.pop());
+            } else if (x.equals("^")) {
+                Double b = stackOperators.pop(), a = stackOperators.pop();
+                stackOperators.push(Math.pow(a, b));
+            } else if (x.equals("-")) {
+                Double b = stackOperators.pop(), a = stackOperators.pop();
+                stackOperators.push(a - b);
+            } else if (x.equals("*")) {
+                stackOperators.push(stackOperators.pop() * stackOperators.pop());
+            } else if (x.equals("/")) {
+                Double b = stackOperators.pop(), a = stackOperators.pop();
+                stackOperators.push(a / b);
+            } else if (x.equals("u-")) {
+                stackOperators.push(-stackOperators.pop());
+            } else {
+                stackOperators.push(Double.valueOf(x));
+            }
+        }
+        return stackOperators.pop();
     }
-    return stackOperators.pop();
-  }
 
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 //    String s = "( -7 ) ^ - 3 + ( ( 5 + 0.3 ) - ( (-7) + (-3) ) )";
-    String s = "78.1 / 3";
-    ExpressionParser n = new ExpressionParser();
-    List<String> expression = n.parse(s);
-    boolean flag = n.flag;
-    if (flag) {
-      for (String x : expression) System.out.print(x + " ");
-      System.out.println();
-      System.out.println(calc(expression));
+        String s = "78.1 / 3";
+        ExpressionParser n = new ExpressionParser();
+        List<String> expression = n.parse(s);
+        boolean flag = n.flag;
+        if (flag) {
+            for (String x : expression) System.out.print(x + " ");
+            System.out.println();
+            System.out.println(calc(expression));
+        }
     }
-  }
 }
--- src/rpn/Sec.java
+++ src/rpn/Sec.java
@@ -1,11 +1,6 @@
 package rpn;
-//(c)Terminator
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-
-
+import java.util.Stack;
 
 final class Expr {
     private final static long LONG_MIN = Long.MIN_VALUE / 10L;
@@ -15,9 +10,9 @@ final class Expr {
     public static long Calc(String s) throws Exception {
         s = s.replaceAll("\\s+", ""); // удалим пробелы, '\n', '\t'...
 
-        if(s.length() == 0)
+        if (s.length() == 0)
             throw new IllegalArgumentException("Строка пуста!");
-        if(! is_rules(s))
+        if (!is_rules(s))
             throw new IllegalArgumentException("Ошибка в расстоновки скобок!");
 
         int[] index = {0};
@@ -26,19 +21,19 @@ final class Expr {
 
     ///////////////////////////////////////////////////////////////////////////
 
-    private static long SubExpr(String s, int[] index){
+    private static long SubExpr(String s, int[] index) {
         boolean neg, loop;
-        long    val, num;
-        char    ch;
+        long val, num;
+        char ch;
         Stack<Character> sops = new Stack<Character>();
-        Stack<Long>      sval = new Stack<Long>();
+        Stack<Long> sval = new Stack<Long>();
 
-        neg  = false;
+        neg = false;
         loop = true;
 
-        for(int i = index[0]; i < s.length(); ){
+        for (int i = index[0]; i < s.length(); ) {
 
-            switch(s.charAt(i)){
+            switch (s.charAt(i)) {
                 case '+':
                 case '/':
                 case '*':
@@ -47,7 +42,7 @@ final class Expr {
                     break;
                 case '-':
 
-                    if((i == 0) || ((i > 0) && is_math(s.charAt(i - 1)))){
+                    if ((i == 0) || ((i > 0) && is_math(s.charAt(i - 1)))) {
                         neg = true;
                         ++i;
                         break;
@@ -59,10 +54,10 @@ final class Expr {
                 case '(':
 
                     index[0] = i + 1;
-                    val      = SubExpr(s, index);
-                    i        = index[0];
+                    val = SubExpr(s, index);
+                    i = index[0];
 
-                    if(neg)
+                    if (neg)
                         val = 0L - val;
                     neg = false;
 
@@ -71,18 +66,18 @@ final class Expr {
                     break;
                 case ')':
                     index[0] = i + 1;
-                    loop     = false;
+                    loop = false;
                     break;
                 default:
 
-                    if(! Character.isDigit(s.charAt(i)))
+                    if (!Character.isDigit(s.charAt(i)))
                         throw new NumberFormatException("Неизвестный символ!");
 
                     index[0] = i;
-                    val      = ToLong(s, index);
-                    i        = index[0];
+                    val = ToLong(s, index);
+                    i = index[0];
 
-                    if(neg)
+                    if (neg)
                         val = 0L - val;
                     neg = false;
 
@@ -91,19 +86,19 @@ final class Expr {
                     break;
             }
 
-            if(! loop)
+            if (!loop)
                 break;
         }
 
 
-        if(sval.size() == 0)
+        if (sval.size() == 0)
             throw new IllegalArgumentException("Ошибка синтаксиса!");
 
         num = sval.get(0);
         sval.remove(0);
 
-        for(int i = 0; (i < sops.size()) && (i < sval.size()); ++i){
-            switch(sops.get(i)){
+        for (int i = 0; (i < sops.size()) && (i < sval.size()); ++i) {
+            switch (sops.get(i)) {
                 case '+':
                     num += sval.get(i);
                     break;
@@ -122,24 +117,24 @@ final class Expr {
 
 
     //приоритетные операции: умножение, деление
-    private static void calc_muldiv(Stack<Long> sval, Stack<Character> sops){
+    private static void calc_muldiv(Stack<Long> sval, Stack<Character> sops) {
         long val;
         char ch;
 
-        while(! sops.isEmpty()){
+        while (!sops.isEmpty()) {
             ch = sops.peek();
-            if((ch == '+') || (ch == '-'))
+            if ((ch == '+') || (ch == '-'))
                 break;
 
             val = sval.peek();
             sval.pop();
-            if(sval.size() == 0)
+            if (sval.size() == 0)
                 throw new IllegalArgumentException("Ошибка синтаксиса!");
 
-            if(ch == '*')
+            if (ch == '*')
                 sval.set(sval.size() - 1, sval.peek() * val);
-            else if(ch == '/'){
-                if(val == 0L)
+            else if (ch == '/') {
+                if (val == 0L)
                     throw new ArithmeticException("Деление на нуль!");
                 sval.set(sval.size() - 1, sval.peek() / val);
             }
@@ -149,14 +144,14 @@ final class Expr {
 
 
     // конвертирование числа из строки
-    private static long ToLong(String s, int[] index){
+    private static long ToLong(String s, int[] index) {
         long n = 0L;
-        int  i;
+        int i;
 
-        for(i = index[0]; i < s.length(); ++i){
-            if(Character.isDigit(s.charAt(i))){
+        for (i = index[0]; i < s.length(); ++i) {
+            if (Character.isDigit(s.charAt(i))) {
 
-                if(n > LONG_MAX || n < LONG_MIN)
+                if (n > LONG_MAX || n < LONG_MIN)
                     throw new NumberFormatException("Переполнение числа!");
 
                 n = n * 10L + (long) (s.charAt(i) - '0');
@@ -168,28 +163,27 @@ final class Expr {
     }
 
     //проверка правильности расстановки скобочных выражений
-    private static boolean is_rules(String s){
+    private static boolean is_rules(String s) {
         int n = 0;
-        for(int i = 0; i < s.length(); ++i){
-            if(s.charAt(i) == '(')
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '(')
                 ++n;
-            else if(s.charAt(i) == ')')
+            else if (s.charAt(i) == ')')
                 --n;
         }
         return (n == 0);
     }
 
 
-    private static boolean is_math(char ch){
+    private static boolean is_math(char ch) {
         return (ch == '+' || ch == '-' ||
                 ch == '*' || ch == '/' || ch == '(');
     }
 }
 
 
-
 class Sec {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         String s = "2*-(-100*100-(-(-(-(10-2)/-2*-3+1)/4)+2)+2012/(1+(-(-2)+3+(-4))+5))+1";
 //        long   n = 2*-(-100*100-(-(-(-(10-2)/-2*-3+1)/4)+2)+2012/(1+(-(-2)+3+(-4))+5))+1;
 
@@ -197,7 +191,7 @@ class Sec {
             long res = Expr.Calc(s);
             System.out.println(res);
 //            System.out.println(n);
-        } catch(Exception e){
+        } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
